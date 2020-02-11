79e4ae6dec725be90b53c66f41c950c80752cc8d
mcampos-quinn <mcq@berkeley.edu>
2020-02-11 10:22:37 -0800

    drive connections/writing working; starting to rethink how to build email as a class

--- clirEmail.py
+++ clirEmail.py
@@ -1,29 +1,26 @@
-import pickle
-import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
+import ast
+import csv
+import os.path
+import pickle
+
+# from letterContent import letterPars as originalPars
 from secrets.other import FOLDER_ID
 
 # https://github.com/googleapis/google-api-python-client
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/documents','https://www.googleapis.com/auth/drive']
+SCOPES = ['https://www.googleapis.com/auth/documents','https://www.googleapis.com/auth/drive',"https://www.googleapis.com/auth/drive.file"]
 
-# The ID of a sample document.
-DOCUMENT_ID = None
 
-# FOLDER_ID = 
-
-def main():
-    """Shows basic usage of the Docs API.
-    Prints the title of a sample document.
-    """
+def login():
+    # Do some login stuff
+    # Return live services for Docs and Drive APIs
     creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
+
     if os.path.exists('secrets/token.pickle'):
         with open('secrets/token.pickle', 'rb') as token:
             creds = pickle.load(token)
@@ -42,69 +39,100 @@ def main():
     g_docs = build('docs', 'v1', credentials=creds)
     g_drive = build('drive','v3',credentials=creds)
 
-    # Retrieve the documents contents from the Docs service.
-    # document = service.documents().get(documentId=DOCUMENT_ID).execute()
-    # files  = g_drive.files().list(q="'{}' in parents".format(FOLDER_ID)).execute()
+    return g_docs, g_drive
 
-    # print('The title of the document is: {}'.format(document.get('title')))
-    for i in range(0,5):
-        title = 'My Document {}'.format(str(i))
-        content = "Hello my name is {}. You killed my father, prepare to die.".format(str(i))
-        print(content)
-        endIndex = len(content)-1
-        print(endIndex)
-        _body = {
-            "title": title,
-            "body": 
-                {
-                    "content": [
-                        # {
-                        #     "startIndex": 0, 
-                        #     "endIndex": 1, 
-                        #     "sectionBreak": {
-                        #         "sectionStyle": {
-                        #             "columnSeparatorStyle": "NONE", 
-                        #             "contentDirection": "LEFT_TO_RIGHT"
-                        #         }
-                        #     }
-                        # },
-                        {
-                            "startIndex": 1,
-                            "endIndex": endIndex,
-                            "paragraph":
-                                {
-                                "elements":[
-                                    {
-                                    # "startIndex": 1,
-                                    # "endIndex": endIndex,
-                                    "textRun":
-                                        {
-                                        "content":"BOLLOCKS"
-                                        }
-                                    }
-                                ]
-                                }
-                        }
-                    ]
-                }
+def insert_paragraph(par,service,docID):
+    print(par)
+    insert = [
+         {
+            'insertText': {
+                'location': {
+                    'index': 1,
+                },
+                'text': par
             }
-        # print(_body)
+        }
+    ]
 
-        new_doc = g_docs.documents().create(body=_body).execute()
-        print(new_doc)
-        _id = new_doc.get("documentId")
-        # parents = g_drive.files().get(fileId=_id).execute()
-        # print(parents)
-        # parent = parents.get('parents')[0]
-        # print(parent)
-        # print(_id)
-        move = g_drive.files().update(fileId=_id,addParents=FOLDER_ID,fields='id, parents').execute()
-        print(move)
+    service.documents().batchUpdate(documentId=docID,body={'requests':insert}).execute()
+
+def parse_paragraphs(person):
+    print(person)
+    originalPars = None
+    print(originalPars)
+    from letterContent import letterPars as originalPars
+    
+    pars = originalPars
+    
+    name = person[0]
+    deceased = person[1]
+    print(name)
+    print(deceased)
+    print(originalPars)
+    # print(deceased == 'y')
+    titles = ast.literal_eval(person[2])
+    recordIDs = person[4]
+    dates = ast.literal_eval(person[5])
+
+    # pars[0] = pars[0].replace('[FULL NAME]',name)
+
+    if deceased == 'y':
+        pars[0] = "To the estate of {} ('THE ESTATE'):".format(name)
+    else:
+        pars[0] = originalPars[0].replace('[FULL NAME]',name)
+        print(pars[0])
+    
+    if dates in ('',[],['']):
+        pars[2] = pars[3] = ''
+    else:
+        pars[3] = '\n'.join(dates)
+    
+    if titles in ('',[],['']):
+        pars[4] = pars[5] = ''
+    else:
+        pars[5] = '\n'.join([title.upper() for title in titles])
+    # recordIDs = pars[-1]
+    # print(pars)
+    originalPars = None
+    del originalPars
 
 
-    # for _file in files.get('files',[]):
-    #     print('found {}'.format(_file.get('name')))
+    return pars
 
+
+def main():
+    g_docs, g_drive = login()
+
+    with open('contacts.csv','r') as f:
+        reader = csv.reader(f)
+        for person in reader:
+            # print(person)
+            paragraphs = parse_paragraphs(person)
+            
+
+            title = 'Email to {}'.format(person[0])
+            _body = {'title':title}
+
+            new_doc = g_docs.documents().create(body=_body).execute()
+            docID = new_doc.get("documentId")
+            # parents = g_drive.files().get(fileId=_id).execute()
+            # print(parents)
+            # parent = parents.get('parents')[0]
+            # print(parent)
+            # print(_id)
+
+            shpargarap = reversed(paragraphs)
+
+            for paragraph in shpargarap:
+                # print(par)
+                if not paragraph in ('',[]):
+                    insert_paragraph(paragraph, g_docs, docID)
+
+            move = g_drive.files().update(fileId=docID,addParents=FOLDER_ID,fields='id, parents').execute()
+            print(move)
+            del paragraphs
+            del shpargarap
+            del person
 
 if __name__ == '__main__':
     main()
---
+++ letterContent.py
@@ -0,0 +1,108 @@
+class Email:
+	"""docstring for ClassName"""
+	def __init__(self,
+				 name,
+				 deceased,
+				 titles,
+				 dates,
+				 recordIDs):
+		
+		self.name = name
+		self.deceased = deceased
+		self.titles = titles
+		self.dates = dates
+
+		self.PAR_1 = 'Dear [FULL NAME],'
+		self.PAR_2 = '''
+					\nThe Berkeley Art Museum and Pacific Film Archive (BAMPFA) has received a grant [link to project blog post] from the Council on Library and Information Resources (CLIR) to digitize nearly 800 audio cassette tapes of guest speakers at the Pacific Film Archive, recorded between 1976 and 1986. As part of this project, we have digitized one or more audio recordings in which you were a participant.\n
+					'''
+		self.PAR_3 = '''
+					The following recording date(s) represent your contributions to this collection that have been digitized:\n
+					'''
+		self.PAR_4 = '' # list of event dates
+		self.PAR_5 = '\n\nThe following film titles were discussed in the relevant recordings:\n\n'
+		self.PAR_6 = '' # List of film titles
+		self.PAR_7 = '''
+					\nAs part of the educational mission of this project, BAMPFA respectfully requests your permission to make these recordings available online via educational partners such as the Internet Archive and/or the BAMPFA website. This project also enables us to preserve these works for researchers into the future as digital formats change. Please note that speakers and/or their heirs retain copyright to their content unless they explicitly waive copyright.\n
+					'''
+		self.PAR_8 = '''
+					Please complete this two-question Google form [link] to record your response to our request.\n
+					'''
+		self.PAR_9 = '''
+					Thank you for participating in this exciting project!\n\n
+					'''
+		self.PAR_10 = 'Sincerely,\n'
+		self.PAR_11 = 'Michael Campos-Quinn and Jason Sanders\n'
+		self.PAR_12 = 'BAMPFA Film Library and Study Center'
+		
+	def paragraphsToList(self):
+		self.allParagraphs = [
+			self.PAR_1,
+			self.PAR_2,
+			self.PAR_3,
+			self.PAR_4,
+			self.PAR_5,
+			self.PAR_6,
+			self.PAR_7,
+			self.PAR_8,
+			self.PAR_9,
+			self.PAR_10,
+			self.PAR_11,
+			self.PAR_12
+			]
+
+
+	def is_deceased(self):
+		self.PAR_1 = "To the estate of {} ('THE ESTATE'):".format(self.name)
+		self.PAR_2 = self.PAR_2.replace(
+			'in which you were a participant.',
+			'in which {} was a participant'.format(self.name)
+			)
+		self.PAR_3 = self.PAR_3.replace(
+			'your contributions',
+			'the contributions of {}'.format(self.name)
+			)
+		self.PAR_7 = self.PAR_7.replace(
+			'your permission',
+			'the permission of THE ESTATE'
+			)
+
+	def test_dates(self):
+		if self.dates in ('',[],['']):
+	        self.PAR_3 = self.PAR_4 = None	
+
+PAR_1 = 'Dear [FULL NAME],'
+
+PAR_2 = '''
+\nThe Berkeley Art Museum and Pacific Film Archive (BAMPFA) has received a grant [link to project blog post] from the Council on Library and Information Resources (CLIR) to digitize nearly 800 audio cassette tapes of guest speakers at the Pacific Film Archive, recorded between 1976 and 1986. As part of this project, we have digitized one or more audio recordings in which you were a participant.\n
+'''
+
+PAR_3 = '''
+The following recording date(s) represent your contributions to this collection that have been digitized:\n
+'''
+
+PAR_4 = '' # list of event dates
+
+PAR_5 = '\n\nThe following film titles were discussed in the above recordings:\n\n'
+
+PAR_6 = '' # List of film titles
+
+PAR_7 = '''
+\nAs part of the educational mission of this project, BAMPFA respectfully requests your permission to make these recordings available online via educational partners such as the Internet Archive and/or the BAMPFA website. This project also enables us to preserve these works for researchers into the future as digital formats change. Please note that speakers retain copyright to their content unless they explicitly waive copyright.\n
+'''
+
+PAR_8 = '''
+Please complete this two-question Google form [link] to record your response to our request.\n
+'''
+
+PAR_9 = '''
+Thank you for participating in this exciting project!\n\n
+'''
+
+PAR_10 = 'Sincerely,\n'
+
+PAR_11 = 'Michael Campos-Quinn and Jason Sanders\n'
+
+PAR_11 = 'BAMPFA Film Library and Study Center'
+
+letterPars = [PAR_1,PAR_2,PAR_3,PAR_4,PAR_5,PAR_6,PAR_7,PAR_8,PAR_9,PAR_10,PAR_11]
