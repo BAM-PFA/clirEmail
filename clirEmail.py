342e6f0a0a09a3d77a4fdf2a99b1b9e9bfa8ba4d
mcampos-quinn <mcq@berkeley.edu>
2020-02-11 10:43:25 -0800

    fuck yeah it works

--- clirEmail.py
+++ clirEmail.py
@@ -7,132 +7,122 @@ import csv
 import os.path
 import pickle
 
-# from letterContent import letterPars as originalPars
+from letterContent import Email
 from secrets.other import FOLDER_ID
 
+# all modified from 
 # https://github.com/googleapis/google-api-python-client
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/documents','https://www.googleapis.com/auth/drive',"https://www.googleapis.com/auth/drive.file"]
+SCOPES = [
+	'https://www.googleapis.com/auth/documents',
+	'https://www.googleapis.com/auth/drive',
+	"https://www.googleapis.com/auth/drive.file"
+	]
 
 
 def login():
-    # Do some login stuff
-    # Return live services for Docs and Drive APIs
-    creds = None
+	# Do some login stuff
+	# Return live services for Docs and Drive APIs
+	creds = None
 
-    if os.path.exists('secrets/token.pickle'):
-        with open('secrets/token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'secrets/credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('secrets/token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+	if os.path.exists('secrets/token.pickle'):
+		with open('secrets/token.pickle', 'rb') as token:
+			creds = pickle.load(token)
+	# If there are no (valid) credentials available, let the user log in.
+	if not creds or not creds.valid:
+		if creds and creds.expired and creds.refresh_token:
+			creds.refresh(Request())
+		else:
+			flow = InstalledAppFlow.from_client_secrets_file(
+				'secrets/credentials.json', SCOPES)
+			creds = flow.run_local_server(port=0)
+		# Save the credentials for the next run
+		with open('secrets/token.pickle', 'wb') as token:
+			pickle.dump(creds, token)
 
-    g_docs = build('docs', 'v1', credentials=creds)
-    g_drive = build('drive','v3',credentials=creds)
+	g_docs = build('docs', 'v1', credentials=creds)
+	g_drive = build('drive','v3',credentials=creds)
 
-    return g_docs, g_drive
+	return g_docs, g_drive
 
 def insert_paragraph(par,service,docID):
-    print(par)
-    insert = [
-         {
-            'insertText': {
-                'location': {
-                    'index': 1,
-                },
-                'text': par
-            }
-        }
-    ]
+	print(par)
+	insert = [
+		 {
+			'insertText': {
+				'location': {
+					'index': 1,
+				},
+				'text': par
+			}
+		}
+	]
 
-    service.documents().batchUpdate(documentId=docID,body={'requests':insert}).execute()
+	service.documents().batchUpdate(
+		documentId=docID,
+		body={'requests':insert}
+		).execute()
 
 def parse_paragraphs(person):
-    print(person)
-    originalPars = None
-    print(originalPars)
-    from letterContent import letterPars as originalPars
-    
-    pars = originalPars
-    
-    name = person[0]
-    deceased = person[1]
-    print(name)
-    print(deceased)
-    print(originalPars)
-    # print(deceased == 'y')
-    titles = ast.literal_eval(person[2])
-    recordIDs = person[4]
-    dates = ast.literal_eval(person[5])
+	# take in a row from the CSV of people to contact
+	# return a completed Email class
+	name = person[0]
+	deceased = person[1]
+	titles = ast.literal_eval(person[2])
+	recordIDs = person[4]
+	dates = ast.literal_eval(person[5])
 
-    # pars[0] = pars[0].replace('[FULL NAME]',name)
+	completeEmail = Email(
+		name=name,
+		deceased=deceased,
+		titles=titles,
+		dates=dates,
+		recordIDs=recordIDs
+		)
 
-    if deceased == 'y':
-        pars[0] = "To the estate of {} ('THE ESTATE'):".format(name)
-    else:
-        pars[0] = originalPars[0].replace('[FULL NAME]',name)
-        print(pars[0])
-    
-    if dates in ('',[],['']):
-        pars[2] = pars[3] = ''
-    else:
-        pars[3] = '\n'.join(dates)
-    
-    if titles in ('',[],['']):
-        pars[4] = pars[5] = ''
-    else:
-        pars[5] = '\n'.join([title.upper() for title in titles])
-    # recordIDs = pars[-1]
-    # print(pars)
-    originalPars = None
-    del originalPars
+	completeEmail.dear()
+	completeEmail.is_deceased()
+	completeEmail.test_dates()
+	completeEmail.test_titles()
+	completeEmail.paragraphs_to_list()
+	completeEmail.reverse_paragraphs()
 
-
-    return pars
+	return completeEmail
 
 
 def main():
-    g_docs, g_drive = login()
+	g_docs, g_drive = login()
 
-    with open('contacts.csv','r') as f:
-        reader = csv.reader(f)
-        for person in reader:
-            # print(person)
-            paragraphs = parse_paragraphs(person)
-            
+	with open('contacts.csv','r') as f:
+		reader = csv.reader(f)
+		for person in reader:
+			# print(person)
+			completeEmail = parse_paragraphs(person)
 
-            title = 'Email to {}'.format(person[0])
-            _body = {'title':title}
+			gDocTitle = 'Email to {}'.format(person[0])
+			_body = {'title':gDocTitle}
 
-            new_doc = g_docs.documents().create(body=_body).execute()
-            docID = new_doc.get("documentId")
-            # parents = g_drive.files().get(fileId=_id).execute()
-            # print(parents)
-            # parent = parents.get('parents')[0]
-            # print(parent)
-            # print(_id)
+			new_doc = g_docs.documents().create(body=_body).execute()
+			docID = new_doc.get("documentId")
+			# parents = g_drive.files().get(fileId=_id).execute()
+			# print(parents)
+			# parent = parents.get('parents')[0]
+			# print(parent)
+			# print(_id)
 
-            shpargarap = reversed(paragraphs)
+			for paragraph in completeEmail.reversedParagraphs:
+				# print(par)
+				if paragraph != None:
+					insert_paragraph(paragraph, g_docs, docID)
 
-            for paragraph in shpargarap:
-                # print(par)
-                if not paragraph in ('',[]):
-                    insert_paragraph(paragraph, g_docs, docID)
-
-            move = g_drive.files().update(fileId=docID,addParents=FOLDER_ID,fields='id, parents').execute()
-            print(move)
-            del paragraphs
-            del shpargarap
-            del person
+			# MOVE THE COMPLETE DOCUMENT TO THE PROJECT FOLDER
+			move = g_drive.files().update(
+				fileId=docID,
+				addParents=FOLDER_ID,
+				fields='id, parents'
+				).execute()
+			print(move)
 
 if __name__ == '__main__':
-    main()
+	main()
--- letterContent.py
+++ letterContent.py
@@ -34,8 +34,11 @@ class Email:
 		self.PAR_10 = 'Sincerely,\n'
 		self.PAR_11 = 'Michael Campos-Quinn and Jason Sanders\n'
 		self.PAR_12 = 'BAMPFA Film Library and Study Center'
+
+		self.allParagraphs = None
+		self.reversedParagraphs = None
 		
-	def paragraphsToList(self):
+	def paragraphs_to_list(self):
 		self.allParagraphs = [
 			self.PAR_1,
 			self.PAR_2,
@@ -51,58 +54,38 @@ class Email:
 			self.PAR_12
 			]
 
+	def reverse_paragraphs(self):
+		self.reversedParagraphs = reversed(self.allParagraphs)
+
+	def dear(self):
+		self.PAR_1 = self.PAR_1.replace('[FULL NAME]',self.name)
 
 	def is_deceased(self):
-		self.PAR_1 = "To the estate of {} ('THE ESTATE'):".format(self.name)
-		self.PAR_2 = self.PAR_2.replace(
-			'in which you were a participant.',
-			'in which {} was a participant'.format(self.name)
-			)
-		self.PAR_3 = self.PAR_3.replace(
-			'your contributions',
-			'the contributions of {}'.format(self.name)
-			)
-		self.PAR_7 = self.PAR_7.replace(
-			'your permission',
-			'the permission of THE ESTATE'
-			)
+		if self.deceased == 'y':
+			self.PAR_1 = "To the estate of {} ('THE ESTATE'):".format(self.name)
+			self.PAR_2 = self.PAR_2.replace(
+				'in which you were a participant.',
+				'in which {} was a participant'.format(self.name)
+				)
+			self.PAR_3 = self.PAR_3.replace(
+				'your contributions',
+				'the contributions of {}'.format(self.name)
+				)
+			self.PAR_7 = self.PAR_7.replace(
+				'your permission',
+				'the permission of THE ESTATE'
+				)
+		else:
+			pass
 
 	def test_dates(self):
 		if self.dates in ('',[],['']):
-	        self.PAR_3 = self.PAR_4 = None	
+			self.PAR_3 = self.PAR_4 = None
+		else:
+			self.PAR_4 = '\n'.join(self.dates)
 
-PAR_1 = 'Dear [FULL NAME],'
-
-PAR_2 = '''
-\nThe Berkeley Art Museum and Pacific Film Archive (BAMPFA) has received a grant [link to project blog post] from the Council on Library and Information Resources (CLIR) to digitize nearly 800 audio cassette tapes of guest speakers at the Pacific Film Archive, recorded between 1976 and 1986. As part of this project, we have digitized one or more audio recordings in which you were a participant.\n
-'''
-
-PAR_3 = '''
-The following recording date(s) represent your contributions to this collection that have been digitized:\n
-'''
-
-PAR_4 = '' # list of event dates
-
-PAR_5 = '\n\nThe following film titles were discussed in the above recordings:\n\n'
-
-PAR_6 = '' # List of film titles
-
-PAR_7 = '''
-\nAs part of the educational mission of this project, BAMPFA respectfully requests your permission to make these recordings available online via educational partners such as the Internet Archive and/or the BAMPFA website. This project also enables us to preserve these works for researchers into the future as digital formats change. Please note that speakers retain copyright to their content unless they explicitly waive copyright.\n
-'''
-
-PAR_8 = '''
-Please complete this two-question Google form [link] to record your response to our request.\n
-'''
-
-PAR_9 = '''
-Thank you for participating in this exciting project!\n\n
-'''
-
-PAR_10 = 'Sincerely,\n'
-
-PAR_11 = 'Michael Campos-Quinn and Jason Sanders\n'
-
-PAR_11 = 'BAMPFA Film Library and Study Center'
-
-letterPars = [PAR_1,PAR_2,PAR_3,PAR_4,PAR_5,PAR_6,PAR_7,PAR_8,PAR_9,PAR_10,PAR_11]
+	def test_titles(self):
+		if self.titles in ('',[],['']):
+			self.PAR_5 = self.PAR_6 = None
+		else:
+			self.PAR_6 = '\n'.join([title.upper() for title in self.titles])
